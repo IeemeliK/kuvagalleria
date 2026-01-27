58b1dd5ae44e5d81d3f519126340b790eadc0bf6
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2026-01-27 17:48:07 +0200

    db connection implementation

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,5 +1,29 @@
 // Package db provides database handling for the app
 package db
 
-func CreateConnection() {
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func CreateConnection() *sql.DB {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
+	db, err := sql.Open("pgx", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.PingContext(context.Background()); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	return db
 }
